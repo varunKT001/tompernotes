b80e7656b8950a38224661ecb0c0a6f95789e2c3
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2021-05-01 12:12:36 +0530

    sasta-notes version 1.0.0 (working)

---
+++ JS/app.js
@@ -0,0 +1,166 @@
+//name of the project : notes taking app
+//author : Varun kumar tiwari 
+console.log('This is sasta-Notes');
+let imp = new Array(100);
+let text = document.getElementById('textArea');
+text.value = '';
+displayNotes();
+
+let btn = document.getElementById('addNote');
+btn.addEventListener('click', function (event) {
+    let notes = localStorage.getItem('notes');
+    let text = document.getElementById('textArea');
+    if (notes == null) {
+        notesArr = [];
+    }
+    else {
+        notesArr = JSON.parse(notes);
+    }
+    if (text.value.trim().length == 0) {
+        alert('Kuch to likh de re baba');
+    }
+    else {
+        notesArr.push(text.value);
+    }
+    localStorage.setItem('notes', JSON.stringify(notesArr));
+    text.value = '';
+    displayNotes();
+})
+function displayNotes() {
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesArr = [];
+    }
+    else {
+        notesArr = JSON.parse(notes);
+    }
+    let html = '';
+    notesArr.forEach(function (element, index) {
+        if (imp[index] == true) {
+            html += `<div class="note" style="border: 2px solid red">
+        <div class="noteIndex" style="margin-bottom: 10px">
+        <h3 style="display: inline">Note-${index + 1}</h3>
+        <button class="btn" style="float: right; margin-bottom: 5px" onclick="openNote(${index})"><span id="close" class="material-icons" style="; font-size: 15px">open_in_full</span></button>
+        <button class="btn" style="float: right; margin-bottom: 5px; margin-right: 2px" onclick="changeToImp(${index})" ondblclick="removeImp(${index})"><span id="imp" class="material-icons" style="; font-size: 15px">label_important</span></button>
+        </div>
+        <div class="content">
+        <p class="noteText">${element}</p>
+        </div>
+        <button id="deleteNote" onclick="deleteNote(${index})" class="btn" >Delete note</button>
+        </div>
+        `;
+        } 
+        else {
+            html += `<div class="note">
+        <div class="noteIndex" style="margin-bottom: 10px">
+        <h3 style="display: inline">Note-${index + 1}</h3>
+        <button class="btn" style="float: right; margin-bottom: 5px" onclick="openNote(${index})"><span id="close" class="material-icons" style="; font-size: 15px">open_in_full</span></button>
+        <button class="btn" style="float: right; margin-bottom: 5px; margin-right: 2px" onclick="changeToImp(${index})" ondblclick="removeImp(${index})"><span id="imp" class="material-icons" style="; font-size: 15px">label_important</span></button>
+        </div>
+        <div class="content">
+        <p class="noteText">${element}</p>
+        </div>
+        <button id="deleteNote" onclick="deleteNote(${index})" class="btn" >Delete note</button>
+        </div>
+        `;
+        }
+    });
+    if (notesArr.length != 0) {
+        document.getElementById('notes').innerHTML = html;
+    }
+    else {
+        document.getElementById('notes').innerHTML = 'You do not have any note right now!';
+        imp = [];
+    }
+}
+function deleteNote(index) {
+    let notes = localStorage.getItem('notes');
+    let notesArr = JSON.parse(notes);
+    notesArr.splice(index, 1);
+    localStorage.setItem('notes', JSON.stringify(notesArr));
+    displayNotes();
+    if (imp[index] === true) {
+        removeImp(index);
+    }
+}
+
+let searchBox = document.getElementById('searchBox');
+searchBox.addEventListener('input', function () {
+    let searchText = searchBox.value;
+    let note = document.getElementsByClassName('note');
+    Array.from(note).forEach(function (element) {
+        let noteText = element.getElementsByTagName('p')[0].innerText;
+        if (noteText.includes(searchText)) {
+            element.style.display = 'block';
+        }
+        else {
+            element.style.display = 'none';
+        }
+    })
+})
+function openNote(index){
+        let notes = localStorage.getItem('notes');
+        let notesArr = JSON.parse(notes);
+        
+        let container = document.getElementsByClassName('container');
+        let bigNote = document.getElementById('bigNotes');
+        if (imp[index] === true) {
+            bigNote.innerHTML = `<div style="margin: 5px">
+            <div class="noteIndex" style="margin:5px 5px">
+            <h3 style="display: inline">Note-${index + 1}</h3>
+            <button class="btn" style="float: right; margin: -2px 0px" onclick="closeNote()"><span id="close" class="material-icons" style="font-size: 15px" >close</span></button>
+            </div>
+            <div class="content" style="height: 355px">
+            <p class="noteText">${notesArr[index]}</p>
+            </div>
+            </div>`;    
+            bigNote.style.border = '2px solid red';
+        } 
+        else {     
+            bigNote.innerHTML = `<div style="margin: 5px">
+            <div class="noteIndex" style="margin:5px 5px">
+            <h3 style="display: inline">Note-${index + 1}</h3>
+            <button class="btn" style="float: right; margin: -2px 0px" onclick="closeNote()"><span id="close" class="material-icons" style="font-size: 15px" >close</span></button>
+            </div>
+            <div class="content" style="height: 355px">
+            <p class="noteText">${notesArr[index]}</p>
+            </div>
+            </div>`;
+        }
+        bigNote.style.display = 'block';
+        container[0].style.opacity = 0.2;
+        bigNote.style.opacity = 1;
+}
+function closeNote(){
+        console.log('fired');
+        let bigNote = document.getElementById('bigNotes');
+        let container = document.getElementsByClassName('container');
+        bigNote.style.display = 'none';
+        bigNote.style.border = '2px solid rgb(57, 216, 84)';
+        container[0].style.opacity = 1;
+}
+let home = document.getElementById('home');
+home.addEventListener('click', function(event){
+    alert('Bas sajawat ke liye lagaya hai!');
+})
+
+function changeToImp(index){
+    for (let i = 0; i < imp.length; i++) {
+        if (i === index) {
+            imp[i] = true;
+        }
+    }
+    displayNotes();
+}
+function removeImp(index){
+    for (let i = 0; i < imp.length; i++) {
+        if (i === index) {
+            imp[i] = false;
+        }
+    }
+    displayNotes();
+}
+
+
+
+
